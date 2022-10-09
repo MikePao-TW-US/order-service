8ff952b0fe637e726c52132fde7454a2cae4ab27
Aman Mehta <amanmehta4411@gmail.com>
2022-10-09 23:32:25 +0530

    spring project setup

---
+++ MultiCartApi/src/test/java/com/multicart/api/MultiCartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicart.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
